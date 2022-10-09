6752db0a78139907adf770d300e5f901c624e5c2
saivignesh2001s <saivignesh2001s@gmail.com>
2022-10-09 12:48:41 +0530

    Add project files.

---
+++ Oct7/Controllers/BookController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Oct7.Models;
+using Hiding;
+using Businesslogic;
+
+namespace Oct7.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        habook nu = null;
+     
+       public BookController()
+        {
+
+            nu = new habook();
+        }
+
+        public List<Bookmodel> ts = new List<Bookmodel>();
+        public List<Bookmodel> sd2()
+        {
+            List<Books> st = nu.booli();
+
+
+            foreach (var item in st)
+            {
+                ts.Add(
+                    new Bookmodel
+                    {
+                        BookId = item.BookId,
+                        Bookname = item.Bookname,
+                        Author = item.Author,
+                        Price = item.Price,
+                        Ctegory = item.Ctegory
+
+                    });
+            }
+            return ts;
+        }
+public ActionResult Index()
+        {
+            List<Bookmodel> tst=sd2().ToList();
+            return View(tst);
+        }
+        public ActionResult AddBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddBook(FormCollection c)
+        {
+            try
+            {
+                Books b = new Books();
+                b.BookId = Convert.ToInt32(Request["BookId"]);
+                b.Bookname = Request["Bookname"];
+                b.Author = Request["Author"];
+                b.Price = Convert.ToDouble(Request["Price"]);
+                b.Ctegory = Request["Ctegory"];
+                bool ans = nu.insert(b);
+                if (ans)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("It is npt valid");
+            }
+        }
+            public ActionResult EditDetails(int id)
+            {
+            List<Bookmodel> sts=sd2().ToList();
+             Bookmodel m = sts.Find(m1 => m1.BookId == id);
+               return View(m);
+           
+
+            }
+        [HttpPost]
+        public ActionResult EditDetails(int id,Bookmodel m)
+        {
+            try { 
+
+                Books b = new Books();
+                b.BookId = m.BookId;
+                b.Bookname = m.Bookname;
+                b.Author = m.Author;
+                b.Price = m.Price;
+                b.Ctegory = m.Ctegory;
+
+                bool ans = nu.updatebook(b,id);
+                if (ans)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+           catch(Exception ex)
+            {
+                return View();
+            }
+
+        }
+        public ActionResult Details(int id)
+        {
+            List<Bookmodel> rt = sd2().ToList();
+            Bookmodel hu = rt.Find(m => m.BookId == id);
+            return View(hu);
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                bool k = nu.delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch(Exception Ex)
+            {
+                return View();
+            }
+        }
+      
+        public ActionResult Find()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Find(string Search)
+        {
+            try
+            {
+                Bookmodel m1 = sd2().ToList().Find(s => s.Bookname == Search);
+                TempData["prodata"] = m1;
+                return RedirectToAction("Showdetail");
+            }
+            catch(Exception ex)
+            {
+                return Index();
+            }
+        }
+        public ActionResult Showdetail()
+        {
+            var n1 = TempData["prodata"];
+            return View(n1);
+
+        }
+
+        
+
+
+
+    }
+
+}
+    
---
+++ Oct7/Models/Bookmodel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Oct7.Models
+{
+    public class Bookmodel
+    {
+        [Required()]
+        private int _bookId;
+        public int BookId
+        {
+            get { return _bookId; }
+            set
+            {
+                if (value > 100)
+                {
+                    _bookId = value;
+                }
+                else
+                {
+                    throw new Exception("It is not recommended");
+                }
+            }
+        }
+        private string _bookname;
+        public string Bookname
+        {
+            get { return _bookname; }
+            set
+            {
+                if (value.Length > 40)
+                {
+                    throw new Exception("It is not valid");
+                }
+                else
+                {
+                    _bookname = value;
+                }
+            }
+
+        }
+        private string _author;
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                if (String.IsNullOrEmpty(value) || value.Length > 30)
+                {
+                    throw new Exception("It is not valid");
+                }
+                else
+                {
+                    _author = value;
+                }
+            }
+        }
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 3000 && value > 0)
+                {
+                    price = value;
+                }
+                else if (price < 0)
+                {
+                    throw new Exception("It cannnot low");
+                }
+
+            }
+        }
+
+        private string ctegory;
+        public string Ctegory
+        {
+            get { return ctegory; }
+            set
+            {
+                if (value.Length <= 10)
+                {
+                    ctegory = value;
+                }
+                else
+                {
+                    throw new Exception("It is not valid");
+                }
+            }
+        }
+    }
+}
